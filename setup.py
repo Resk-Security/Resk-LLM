f2eba283975885ab672680b185e5dd6a465fa7ec
resk <61885397+zengdard@users.noreply.github.com>
2025-05-21 09:28:00 +0200

    correction version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("LICENSE.txt", "r", encoding="utf-8") as f:
 
 setup(
     name="resk-llm",
-    version="1.0.3",
+    version="1.0.7",
     author="Resk",
     author_email="nielzac@proton.me",
     description="Resk-LLM is a robust Python library designed to enhance security and manage context when interacting with LLM APIs. It provides a protective layer for API calls, safeguarding against common vulnerabilities and ensuring optimal performance.",
