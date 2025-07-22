560ba66c6d0e205c22dde7d7ee5899dd3107b9a7
resk <jgn54350@aldecis.com>
2025-07-22 13:27:26 +0200

    pyproject version maj

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("LICENSE.txt", "r", encoding="utf-8") as f:
 
 setup(
     name="resk-llm",
-    version="2.0.0",
+    version="2.0.10",
     author="Resk",
     author_email="contact@resk.fr",
     description="Resk-LLM is a robust Python library designed to enhance security and manage context when interacting with LLM APIs. It provides a protective layer for API calls, safeguarding against common vulnerabilities and ensuring optimal performance.",
